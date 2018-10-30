b6aa617a508a84c1b405c0d5d25cd21262fde42a
unknown <shashishekhar1001@gmail.com>
2018-10-30 17:26:49 +0530

    Reading files from data folder

--- chatbot_database.py
+++ chatbot_database.py
@@ -1,4 +1,8 @@
 import sqlite3
+import os
+
+current_directory = os.getcwd()
+data_directory = os.path.join(current_directory, 'data')
 
 sqlite_transaction = [] 
 connection = sqlite3.connect('{}.db'.format('my_db'))
@@ -9,6 +13,13 @@ def create_table():
         question TEXT PRIMARY KEY, answer TEXT
     )""")
 
-
 if __name__ == "__main__":
     create_table()
+    for file in os.listdir(data_directory):
+        file_path =  os.path.join(data_directory, file)
+        with open(file_path, buffering=1000, encoding='utf8') as f:
+            read_data = f.read()
+            print(read_data)
+            print("\n" * 20)
+
+        
